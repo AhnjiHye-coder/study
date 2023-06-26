98382c4b7cee2fd20c7537db59097c51d4ebf2cc
AhnJiHye-coder <dkswlgP224677@gmail.com>
2023-06-26 22:53:05 +0900

    Study04 추가

--- Study04/src/Quiz04/Solution3.java
+++ Study04/src/Quiz04/Solution3.java
@@ -1,28 +1,23 @@
 package Quiz04;
 
-// 문자열 묶기
+// 문자열 묶기 181855
 // 금,토
 
-import java.util.Arrays;
+import java.util.HashMap;
+
 class Solution {
     public int solution(String[] strArr) {
+    	HashMap<Integer, Integer> coun = new HashMap<>();
         int answer = 0;
        
         
-        for(int i = 0; i < strArr.length; i++){ // 배열에서
-            if(strArr[i] == null){       // null이면
-            	return -1;//  index++해서 다음 index이동                
-                }// end of if
-            else if(strArr[i] == strArr[i]){
-                 answer++; 
-            }
-            else {
-            	answer++;
-            }
-          
-          
-            }// end of for
-
+        for(String str : strArr){ // 배열에서
+        	coun.put(str.length(), coun.getOrDefault(str.length(), 0) + 1);
+        }// end of for
+        
+        for(int count : coun.values()) {
+        	answer = Math.max(answer, count);
+        }
         return answer;
     }
 }
